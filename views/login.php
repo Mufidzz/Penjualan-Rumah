6d91460c133f1c6cc943d8ba6af43f1150da0952
Mufidzz <dmzmzzz@users.noreply.github.com>
2019-05-08 00:31:50 +0700

    Login, Register Added

---
+++ views/login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/global.css"/>
+    <link rel="stylesheet" type="text/css" href="css/login.css"/>
+    <link rel="stylesheet" type="text/css" href="css/m/login.css"/>
+    <link rel="stylesheet" type="text/css" href="css/navbar.css"/>
+    <link rel="stylesheet" type="text/css" href="css/m/navbar.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="header-container">
+        <div class="left-side-container">
+            <button class="btnBack">Back</button>
+        </div>
+    </div>
+    <div class="content-container">
+        <label>Sign in</label>
+        <form>
+            <input type="text" placeholder="username">
+            <input type="password" placeholder="password">
+            <button>Sign in</button>
+        </form>
+        <a href="#">not yet member? register</a>
+    </div>
+</body>
+</html>
---
+++ views/register.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/global.css"/>
+    <link rel="stylesheet" type="text/css" href="css/register.css"/>
+    <link rel="stylesheet" type="text/css" href="css/m/register.css"/>
+    <link rel="stylesheet" type="text/css" href="css/navbar.css"/>
+    <link rel="stylesheet" type="text/css" href="css/m/navbar.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="header-container">
+        <div class="left-side-container">
+            <button class="btnBack">Back</button>
+        </div>
+    </div>
+    <div class="content-container">
+        <label>Sign Up</label>
+        <form>
+            <input type="text" placeholder="name">
+            <input type="text" placeholder="username">
+            <input type="password" placeholder="password">
+            <input type="number" placeholder="whatsapp number">
+            <button>Sign in</button>
+        </form>
+        <a href="#">already member? sign in</a>
+    </div>
+</body>
+</html>
